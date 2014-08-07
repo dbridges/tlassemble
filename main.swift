cacaf8ebd016bf0856d1c0d6d25fa740f46caa03
Daniel Bridges <bridges2@gmail.com>
2014-08-06 20:09:25 -0700

    basic stuff.

--- main.swift
+++ main.swift
@@ -1,5 +1,25 @@
 import Cocoa
 
-println("Hello Swift")
+func exitWithError(error: String) {
+    println(error)
+    exit(EXIT_FAILURE)
+}
 
-printlns(5)
+func getImageFiles(srcDir: String) -> [String]? {
+    let fileManager = NSFileManager.defaultManager()
+    if let contents = fileManager.contentsOfDirectoryAtPath(srcDir, error: nil) as [String]? {
+        return contents.filter({$0.lowercaseString.hasSuffix(".jpeg") || 
+                                $0.lowercaseString.hasSuffix(".jpg") || 
+                                $0.lowercaseString.hasSuffix(".png")})
+    } else {
+        return nil
+    }
+}
+
+func main() {
+    if let files = getImageFiles("./test") {
+
+    }
+}
+
+main()
